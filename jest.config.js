128a2638180b31b70e8f34ead9852373b69a46b4
boasbabs <babalolasimeon@gmail.com>
2019-05-04 08:57:58 +0100

    test(enzyme): add jest setup and configuration

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    setupFiles: ["<rootDir>/src/__test__/setupTests.js"],
+    moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+      "\\.(css|less)$": "<rootDir>/src/__mocks__/styleMock.js"
+    },
+    clearMocks: true
+  };
